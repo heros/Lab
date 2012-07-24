fcd1dbdcef41d979d0414340248c014e76d136d8
cooler-SAI <cooler19860212@yahoo.com>
2012-07-24 20:23:11 +0400

    pre release clearn up, errors fix...

--- !Milestone/_version.sql
+++ !Milestone/_version.sql
@@ -1,2 +1,2 @@
-UPDATE `version` SET `db_version`=('SkyFire DB NG Release & "patch" SmartAI Lab 406.02');
+UPDATE `version` SET `db_version`=('SkyFire DB NG Release & "patch" SmartAI Lab 406.03');
 
