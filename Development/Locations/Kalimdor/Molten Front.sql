c9d49ff7c1a617af6ba0c15fef1800fd14cca3c2
cooler-SAI <cooler19860212@yahoo.com>
2012-07-25 22:39:28 +0400

    more scripts for Molten Front zone...moving forward

--- Development/Locations/Kalimdor/Molten Front.sql
+++ Development/Locations/Kalimdor/Molten Front.sql
@@ -82,11 +82,103 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,0,0,4,0,100,1,0,0,0,0,11,61684,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Dash on Aggro'),
 (@ENTRY,0,1,0,0,0,100,0,4000,6000,16000,19000,11,101413,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Searing Webs');
 
+-- Cinderweb Broodling
+SET @ENTRY := 53753;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,100,1,0,0,0,0,11,61684,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Dash on Aggro');
 
+-- Cinderweb Clutchkeeper
+SET @ENTRY := 53656;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,4000,6000,15000,20000,11,100700,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Parasitic Injection');
 
+-- Cinderweb Creeper
+SET @ENTRY := 52648;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,3000,4500,17000,22000,11,98054,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Flame Injection'),
+(@ENTRY,0,1,0,4,0,100,1,0,0,0,0,11,61684,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Dash on Aggro');
 
+-- Cinderweb Queen <Lieutenant of Flame>
+SET @ENTRY := 53759;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,9,0,100,0,10,40,15000,19000,11,100017,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Cinder Web on Close'),
+(@ENTRY,0,1,0,9,0,100,0,5,45,8000,9000,11,99996,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Flame Spit on Close'),
+(@ENTRY,0,2,0,0,0,100,0,2000,4500,22000,25000,11,99991,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Writhing Injection');
 
+-- Cinderweb Spinner
+SET @ENTRY := 52981;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,5000,6000,22000,26000,11,97959,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Entangling Net'),
+(@ENTRY,0,1,0,4,0,100,1,0,0,0,0,11,61684,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Dash on Aggro');
 
+-- Deth'tilac <The Smouldering Darkness>
+SET @ENTRY := 54322;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,2000,4500,30000,35000,11,101497,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Aura of Deth\'spair'),
+(@ENTRY,0,1,0,2,0,100,1,0,10,0,0,11,101226,0,0,0,0,0,5,0,0,0,0,0,0,0,'Cast Deth Strike at 10% HP'),
+(@ENTRY,0,2,0,0,0,100,0,6000,8000,15000,22000,11,101261,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Deth Metal');
+
+-- Devout Harbinger <Lieutenant of Flame>
+SET @ENTRY := 53834;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,100,1,0,0,0,0,22,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 1 on Aggro'),
+(@ENTRY,0,1,0,4,1,100,1,0,0,0,0,21,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Stop Moving on Aggro'),
+(@ENTRY,0,2,0,4,1,100,1,0,0,0,0,11,100229,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Incinerate on Aggro'),
+(@ENTRY,0,3,0,9,1,100,0,0,40,3400,4700,11,100229,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Incinerate'),
+(@ENTRY,0,4,0,9,1,100,0,40,100,0,0,21,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Start Moving when not in Incinerate Range'),
+(@ENTRY,0,5,0,9,1,100,0,10,15,0,0,21,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Stop Moving at 15 Yards'),
+(@ENTRY,0,6,0,9,1,100,0,0,40,0,0,21,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Stop Moving when in Incinerate Range'),
+(@ENTRY,0,7,0,3,1,100,0,0,15,0,0,22,2,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 2 at 15% Mana'),
+(@ENTRY,0,8,0,3,2,100,0,0,15,0,0,21,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Start Moving at 15% Mana'),
+(@ENTRY,0,9,0,3,2,100,0,30,100,100,100,22,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 1 When Mana is above 30%'),
+(@ENTRY,0,10,0,0,1,100,0,9000,12000,25000,28000,11,100237,1,0,0,0,0,5,0,0,0,0,0,0,0,'Cast Creeping Inferno');
+
+-- Druid of the Flame
+SET @ENTRY := 52661;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,2000,4500,12000,13000,11,31279,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Swipe'),
+(@ENTRY,0,1,0,0,0,100,0,7000,9000,17000,22000,11,18543,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Flame Lash');
+
+-- Druid of the Flame
+SET @ENTRY := 52122;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,100,1,0,0,0,0,11,97684,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Force of Flame on Aggro'),
+(@ENTRY,0,1,0,0,0,100,0,5000,7000,12000,14000,11,17273,1,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Pyroblast'),
+(@ENTRY,0,2,0,2,0,100,1,0,30,0,0,11,97903,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Rage of Ragnaros at 30% HP'),
+(@ENTRY,0,3,0,0,0,100,0,4000,9000,10000,18000,11,13878,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Scorch');
+
+-- Druid of the Flame
+SET @ENTRY := 52872;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,1,0,0,0,0,22,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 1 on Aggro'),
+(@ENTRY,0,1,0,0,1,100,0,5000,7000,12000,14000,11,17273,1,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Pyroblast'),
+(@ENTRY,0,2,0,0,1,100,0,4000,9000,10000,18000,11,13878,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Scorch'),
+(@ENTRY,0,3,0,2,1,100,1,0,55,0,0,22,2,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 2 at 55% HP'),
+(@ENTRY,0,4,0,2,2,100,1,0,30,0,0,11,97903,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Rage of Ragnaros at 30% HP'),
+(@ENTRY,0,5,0,2,2,100,1,0,50,0,0,11,97679,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Flame Scorpion Form at 50% HP'),
+(@ENTRY,0,6,0,0,2,100,0,2000,4500,9000,13000,11,31289,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Claw'),
+(@ENTRY,0,7,0,0,2,100,0,6000,8000,17000,21000,11,32093,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Poison Spit'),
+(@ENTRY,0,8,0,7,2,100,1,0,0,0,0,22,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Reset on Evade');
 
 
 
