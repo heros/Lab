ddaf3589d9f9b07ae057ce8da0bac507512b2f56
cooler-SAI <cooler19860212@yahoo.com>
2012-08-08 22:24:51 +0400

    added missed script

--- Development/Locations/Eastern Kingdoms/Silverpine Forest.sql
+++ Development/Locations/Eastern Kingdoms/Silverpine Forest.sql
@@ -781,6 +781,13 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,0,0,0,0,100,0,7000,9000,19000,20000,11,13884,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Withering Poison'),
 (@ENTRY,0,1,0,9,0,100,0,0,5,12000,14000,11,87081,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Strike on Close');
 
+-- Tirisfal Bear
+SET @ENTRY := 51962;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,0,2000,4500,12000,14000,11,31279,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Swipe');
+
 -- Thule Ravenclaw
 SET @ENTRY := 47023;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
